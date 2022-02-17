76b67e4f81280e4545047b86f98a137b5a121976
teamclouday <teamclouday@gmail.com>
2022-02-17 12:57:44 -0500

    add scripts

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -d build ]
+then
+    mkdir build
+fi
+cd build
+if [ $# -eq 0 ]
+then
+# default use c++11
+cmake .. -DCMAKE_CXX_FLAGS=-std=c++11
+else
+cmake .. -DCMAKE_CXX_FLAGS=-std=c++$1
+fi
+make
+cd ..
+./bin/run
