0618ed5b89ef3b47520a3fc9dd87641f36fb8315
teamclouday <teamclouday@gmail.com>
2022-02-17 20:27:29 -0500

    improve script

--- run.ps1
+++ run.ps1
@@ -4,8 +4,10 @@ if (-not (Test-Path -Path $BuildPath))
 {
     New-Item $BuildPath -ItemType Directory
 }
-Start-Process -NoNewWindow -Wait -FilePath "cmake.exe" -WorkingDirectory $BuildPath -ArgumentList "..","-DCMAKE_CXX_FLAGS=/std:c++$cppversion"
-Start-Process -NoNewWindow -Wait -FilePath "cmake.exe" -WorkingDirectory $BuildPath -ArgumentList "--build",".","--config","Release"
+Set-Location $BuildPath
+& "cmake.exe" ".." "-DCMAKE_CXX_FLAGS=/std:c++$cppversion"
+& "cmake.exe" "--build" "." "--config" "Release"
+Set-Location ..
 Write-Host "==========="
 Write-Host " Executing"
 Write-Host "==========="
